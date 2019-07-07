430a6d7547166367de9de4abd559958bced0c74a
Kamille Delgardo <artemis@pop-os.localdomain>
2019-07-07 05:06:44 -0700

    mine spacing

--- projects/delgarka/dominion/dominion.c
+++ projects/delgarka/dominion/dominion.c
@@ -770,20 +770,17 @@ int cardEffect(int card, int choice1, int choice2, int choice3, struct gameState
     case mine:
       j = state->hand[currentPlayer][choice1];  //store card we will trash
 
-      if (state->hand[currentPlayer][choice1] < copper || state->hand[currentPlayer][choice1] > gold)
-	{
-	  return -1;
-	}
+      if (state->hand[currentPlayer][choice1] < copper || state->hand[currentPlayer][choice1] > gold) {
+        return -1;
+      }
 
-      if (choice2 > treasure_map || choice2 < curse)
-	{
-	  return -1;
-	}
+      if (choice2 > treasure_map || choice2 < curse) {
+        return -1;
+      }
 
-      if ( (getCost(state->hand[currentPlayer][choice1]) + 3) > getCost(choice2) )
-	{
-	  return -1;
-	}
+      if ((getCost(state->hand[currentPlayer][choice1]) + 3) > getCost(choice2)) {
+        return -1;
+      }
 
       gainCard(choice2, state, 2, currentPlayer);
 
@@ -791,14 +788,12 @@ int cardEffect(int card, int choice1, int choice2, int choice3, struct gameState
       discardCard(handPos, currentPlayer, state, 0);
 
       //discard trashed card
-      for (i = 0; i < state->handCount[currentPlayer]; i++)
-	{
-	  if (state->hand[currentPlayer][i] == j)
-	    {
-	      discardCard(i, currentPlayer, state, 0);
-	      break;
-	    }
-	}
+      for (i = 0; i < state->handCount[currentPlayer]; i++) {
+        if (state->hand[currentPlayer][i] == j) {
+          discardCard(i, currentPlayer, state, 0);
+          break;
+        }
+      }
 
       return 0;
 
